11f4a1eb632739a4e99d0b7de7d7fd281a997526
Simon Alling <alling.simon@gmail.com>
2017-11-13 12:13:36 +0100

    Version number 2.2.4 and date 2017-11-13

--- better-sweclockers.user.js
+++ better-sweclockers.user.js
@@ -3,7 +3,7 @@
 // @namespace       http://alling.se
 //
 //                  *** Don't forget to update version below as well! ***
-// @version         2.2.3
+// @version         2.2.4
 //                  *** Don't forget to update version below as well! ***
 //
 // @match           http://*.sweclockers.com/*
@@ -26,7 +26,7 @@ var Better_SweClockers = (function() {
 "use strict";
 
 // Needed for update check. Remember to update!
-var version = "2.2.3";
+var version = "2.2.4";
 
 // "Constants"
 var ABOVE_STANDARD_CONTROL_PANEL = 0;
