e7559c3bd5937a5b034c4899a7c69b6385278fef
Simon Alling <alling.simon@gmail.com>
2019-01-26 13:04:40 +0100

    Version number 3.4.0

--- config/userscript.ts
+++ config/userscript.ts
@@ -1,7 +1,7 @@
 export default {
     id: "better-sweclockers",
     name: "Better SweClockers",
-    version: "3.3.0",
+    version: "3.4.0",
     description: "Extra functionality and improved usability for the SweClockers website.",
     author: "Simon Alling",
     hostname: "sweclockers.com",
