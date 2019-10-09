64b8146aba67d365a16a6faa658718f2151295e9
Simon Alling <alling.simon@gmail.com>
2019-10-09 07:49:48 +0200

    Use lowercase names for operations

--- src/operations.ts
+++ src/operations.ts
@@ -15,27 +15,27 @@ import {
     isReadingThread,
     mayHaveJustSubmittedForumPost,
 } from "./environment";
-import INSERT_PREFERENCES_MENU from "./operations/insert-preferences-menu";
-import INSERT_PREFERENCES_LINK from "./operations/insert-preferences-link";
-import INSERT_PREFERENCES_SHORTCUT from "./operations/insert-preferences-shortcut";
-import INSERT_WEB_SEARCH_BUTTON from "./operations/insert-web-search-button";
-import INSERT_EDITING_TOOLS from "./operations/insert-editing-tools";
-import INSERT_HEADING_TOOLBAR_BUTTON from "./operations/insert-heading-toolbar-button";
-import INSERT_TABLE_TOOLBAR_BUTTON from "./operations/insert-table-toolbar-button";
-import INSERT_TEXTAREA_SIZE_TOGGLE from "./operations/insert-textarea-size-toggle";
-import INSERT_LINK_TO_TOP from "./operations/insert-link-to-top";
-import INSERT_PM_LINKS from "./operations/insert-pm-links";
-import FIX_MOBILE_LINKS from "./operations/fix-mobile-links";
-import INSERT_QUOTE_SIGNATURE_BUTTONS from "./operations/insert-quote-signature-buttons";
-import PREVENT_ACCIDENTAL_SIGNOUT from "./operations/prevent-accidental-signout";
-import * as PREVENT_ACCIDENTAL_UNLOAD from "./operations/prevent-accidental-unload";
-import ADAPT_CORRECTIONS_LINK from "./operations/adapt-corrections-link";
-import REPLACE_FOLLOWED_THREADS_LINK from "./operations/replace-followed-threads-link";
-import MANAGE_CARET_POSITION from "./operations/manage-caret-position";
-import REMOVE_MOBILE_SITE_DISCLAIMER from "./operations/remove-mobile-site-disclaimer";
-import * as AUTOSAVE_DRAFT from "./operations/autosave-draft";
-import * as KEYBOARD_SHORTCUTS_EDIT_MODE from "./operations/keyboard-shortcuts/edit-mode";
-import MOUSETRAP_PREPARATIONS from "./operations/mousetrap-preparations";
+import insertPreferencesMenu from "./operations/insert-preferences-menu";
+import insertPreferencesLink from "./operations/insert-preferences-link";
+import insertPreferencesShortcut from "./operations/insert-preferences-shortcut";
+import insertWebSearchButton from "./operations/insert-web-search-button";
+import insertEditingTools from "./operations/insert-editing-tools";
+import insertHeadingToolbarButton from "./operations/insert-heading-toolbar-button";
+import insertTableToolbarButton from "./operations/insert-table-toolbar-button";
+import insertTextareaSizeToggle from "./operations/insert-textarea-size-toggle";
+import insertLinkToTop from "./operations/insert-link-to-top";
+import insertPMLinks from "./operations/insert-pm-links";
+import fixMobileLinks from "./operations/fix-mobile-links";
+import insertQuoteSignatureButtons from "./operations/insert-quote-signature-buttons";
+import preventAccidentalSignout from "./operations/prevent-accidental-signout";
+import * as preventAccidentalUnload from "./operations/prevent-accidental-unload";
+import adaptCorrectionsLink from "./operations/adapt-corrections-link";
+import replaceFollowedThreadsLink from "./operations/replace-followed-threads-link";
+import manageCaretPosition from "./operations/manage-caret-position";
+import removeMobileSiteDisclaimer from "./operations/remove-mobile-site-disclaimer";
+import * as autosaveDraft from "./operations/autosave-draft";
+import * as keyboardShortcutsEditMode from "./operations/keyboard-shortcuts/edit-mode";
+import performMousetrapPreparations from "./operations/mousetrap-preparations";
 import * as DarkTheme from "./operations/dark-theme";
 import * as Proofreading from "./operations/proofreading";
 
@@ -58,7 +58,7 @@ const OPERATIONS: readonly Operation[] = [
         description: "insert preferences menu",
         condition: () => isOnBSCPreferencesPage,
         selectors: { body: "body" },
-        action: INSERT_PREFERENCES_MENU,
+        action: insertPreferencesMenu,
     }),
     new IndependentOperation({
         description: "disable scroll restoration",
@@ -76,12 +76,12 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             _actionButtons: SELECTOR.actionButtons,
         },
-        action: PREVENT_ACCIDENTAL_UNLOAD.postOrMessage,
+        action: preventAccidentalUnload.postOrMessage,
     }),
     new IndependentOperation({
         description: "prevent accidental unload (corrections)",
         condition: () => isReadingEditorialContent && Preferences.get(P.advanced._.prevent_accidental_unload),
-        action: PREVENT_ACCIDENTAL_UNLOAD.corrections,
+        action: preventAccidentalUnload.corrections,
     }),
     new DependentOperation({
         description: "insert web search button",
@@ -90,7 +90,7 @@ const OPERATIONS: readonly Operation[] = [
             searchFieldInput: SELECTOR.searchFieldInput,
             searchFieldWrapper: SELECTOR.searchFieldWrapper,
         },
-        action: INSERT_WEB_SEARCH_BUTTON,
+        action: insertWebSearchButton,
     }),
     new DependentOperation({
         description: "manage caret position in textarea",
@@ -100,25 +100,25 @@ const OPERATIONS: readonly Operation[] = [
             // The market doesn't have a dedicated preview button; its save button fills that purpose in this context.
             previewButton: isInEditMode_market ? SELECTOR.saveButton : SELECTOR.previewButton,
         },
-        action: MANAGE_CARET_POSITION,
+        action: manageCaretPosition,
     }),
     new DependentOperation({
         description: "remove mobile site disclaimer",
         condition: () => isInEditMode_forum,
         selectors: { textarea: SELECTOR.textarea },
-        action: REMOVE_MOBILE_SITE_DISCLAIMER,
+        action: removeMobileSiteDisclaimer,
     }),
     new DependentOperation({
         description: "insert editing tools",
         condition: () => isInEditMode && Preferences.get(P.editing_tools._.enable),
         selectors: { textarea: SELECTOR.textarea },
-        action: INSERT_EDITING_TOOLS,
+        action: insertEditingTools,
     }),
     new DependentOperation({
         description: "insert editing tools in quick reply form",
         condition: () => isReadingThread && Preferences.get(P.editing_tools._.enable) && Preferences.get(P.editing_tools._.in_quick_reply_form),
         selectors: { textarea: SELECTOR.textarea },
-        action: INSERT_EDITING_TOOLS,
+        action: insertEditingTools,
     }),
     new DependentOperation({
         description: "insert heading toolbar button",
@@ -127,7 +127,7 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             strikeButton: SELECTOR.textareaToolbarStrikeButton,
         },
-        action: INSERT_HEADING_TOOLBAR_BUTTON,
+        action: insertHeadingToolbarButton,
     }),
     new DependentOperation({
         description: "insert table toolbar button",
@@ -136,7 +136,7 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             unorderedListButton: SELECTOR.textareaToolbarUnorderedListButton,
         },
-        action: INSERT_TABLE_TOOLBAR_BUTTON,
+        action: insertTableToolbarButton,
     }),
     new DependentOperation({
         description: "insert textarea size toggle",
@@ -145,14 +145,14 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             toolbarInner: SELECTOR.textareaToolbarInner,
         },
-        action: INSERT_TEXTAREA_SIZE_TOGGLE,
+        action: insertTextareaSizeToggle,
     }),
     new DependentOperation({
         // Should be inserted before dark theme toggle because they should be in that order.
         description: "insert preferences shortcut",
         condition: () => !isOnBSCPreferencesPage && Preferences.get(P.general._.insert_preferences_shortcut),
         selectors: { topMenu: SELECTOR.topMenu },
-        action: INSERT_PREFERENCES_SHORTCUT,
+        action: insertPreferencesShortcut,
     }),
     new DependentOperation({
         description: "insert dark theme toggle",
@@ -164,43 +164,43 @@ const OPERATIONS: readonly Operation[] = [
         description: "prevent accidental signout",
         condition: () => Preferences.get(P.advanced._.prevent_accidental_signout),
         selectors: { signoutButtonOrSigninSection: SELECTOR.signinSectionOr("#" + SITE.ID.signoutButton ) },
-        action: PREVENT_ACCIDENTAL_SIGNOUT,
+        action: preventAccidentalSignout,
     }),
     new DependentOperation({
         description: "insert preferences link",
         condition: () => isOnSweclockersSettingsPage,
         selectors: { settingsNavigation: SELECTOR.settingsNavigation },
-        action: INSERT_PREFERENCES_LINK,
+        action: insertPreferencesLink,
     }),
     new DependentOperation({
         description: "insert link to top",
         condition: () => isReadingThread && Preferences.get(P.forum_threads._.insert_link_to_top),
-        action: INSERT_LINK_TO_TOP,
+        action: insertLinkToTop,
         selectors: { parent: SELECTOR.listBulkActions },
     }),
     new IndependentOperation({
         description: "insert PM links",
         condition: () => Preferences.get(P.forum_threads._.insert_pm_links),
-        action: INSERT_PM_LINKS,
+        action: insertPMLinks,
         waitForDOMContentLoaded: true,
     }),
     new IndependentOperation({
         description: "fix mobile links",
         condition: () => isReadingThread && Preferences.get(P.forum_threads._.fix_mobile_links),
-        action: FIX_MOBILE_LINKS,
+        action: fixMobileLinks,
         waitForDOMContentLoaded: true,
     }),
     new DependentOperation({
         description: "insert quote signature buttons",
         condition: () => isReadingThread && Preferences.get(P.forum_threads._.quote_signature_buttons),
-        action: INSERT_QUOTE_SIGNATURE_BUTTONS,
+        action: insertQuoteSignatureButtons,
         selectors: { quickReplyForm: SELECTOR.quickReplyForm },
     }),
     new DependentOperation({
         description: "adapt corrections link to work with improved corrections",
         condition: () => isReadingEditorialContent && Preferences.get(P.general._.improved_corrections),
         selectors: { correctionsLink: "#" + SITE.ID.correctionsLink },
-        action: ADAPT_CORRECTIONS_LINK,
+        action: adaptCorrectionsLink,
     }),
     new IndependentOperation({
         description: "enable proofreading",
@@ -227,7 +227,7 @@ const OPERATIONS: readonly Operation[] = [
         description: "replace followed threads link with a link to my posts",
         condition: () => Preferences.get(P.general._.replace_followed_threads_link),
         selectors: { followedThreadsLinkTextOrSigninSection: SELECTOR.signinSectionOr(SELECTOR.followedThreadsLinkText) },
-        action: REPLACE_FOLLOWED_THREADS_LINK,
+        action: replaceFollowedThreadsLink,
     }),
     new DependentOperation({
         description: "enable autosave draft watchdog",
@@ -237,25 +237,25 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             toolbarInner: SELECTOR.textareaToolbarInner,
         },
-        action: AUTOSAVE_DRAFT.manageAutosaveWatchdog,
+        action: autosaveDraft.manageAutosaveWatchdog,
     }),
     new DependentOperation({
         description: "delete any obsolete autosaved draft",
         condition: () => mayHaveJustSubmittedForumPost && Preferences.get(P.edit_mode._.autosave_draft),
         selectors: { post: SELECTOR.linkedForumPost },
-        action: AUTOSAVE_DRAFT.clearAutosavedDraftIfObsolete,
+        action: autosaveDraft.clearAutosavedDraftIfObsolete,
     }),
     new IndependentOperation({
         description: "delete any leftover autosaved draft",
         condition: () => isFalse(Preferences.get(P.edit_mode._.autosave_draft)),
-        action: AUTOSAVE_DRAFT.clearAutosavedDraft,
+        action: autosaveDraft.clearAutosavedDraft,
     }),
 
     // Keyboard shortcuts
     new IndependentOperation({
         description: "perform Mousetrap preparations",
         condition: () => ALWAYS,
-        action: MOUSETRAP_PREPARATIONS,
+        action: performMousetrapPreparations,
         waitForDOMContentLoaded: true,
     }),
     new DependentOperation({
@@ -265,7 +265,7 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             saveButton: SELECTOR.saveButton,
         },
-        action: KEYBOARD_SHORTCUTS_EDIT_MODE.submit,
+        action: keyboardShortcutsEditMode.submit,
         waitForDOMContentLoaded: true,
     }),
     new DependentOperation({
@@ -275,7 +275,7 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             previewButton: SELECTOR.previewButton,
         },
-        action: KEYBOARD_SHORTCUTS_EDIT_MODE.preview,
+        action: keyboardShortcutsEditMode.preview,
         waitForDOMContentLoaded: true,
     }),
     new DependentOperation({
@@ -285,7 +285,7 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             saveButton: SELECTOR.saveButtonQuickReply,
         },
-        action: KEYBOARD_SHORTCUTS_EDIT_MODE.submit,
+        action: keyboardShortcutsEditMode.submit,
     }),
     new DependentOperation({
         description: "add quick reply keyboard shortcut (preview)",
@@ -294,7 +294,7 @@ const OPERATIONS: readonly Operation[] = [
             textarea: SELECTOR.textarea,
             previewButton: SELECTOR.previewButtonQuickReply,
         },
-        action: KEYBOARD_SHORTCUTS_EDIT_MODE.preview,
+        action: keyboardShortcutsEditMode.preview,
     }),
 ];
 
