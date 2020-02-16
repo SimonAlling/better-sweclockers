d441c210d5f8bbe5d4061d15efed108c933a64b3
Simon Alling <alling.simon@gmail.com>
2020-02-16 17:59:01 +0100

    Fix incorrect indentation

--- src/operations/remember-location-in-market.ts
+++ src/operations/remember-location-in-market.ts
@@ -11,18 +11,18 @@ export default (e: {
     const cityInput = e.city as HTMLInputElement;
     const regionSelect = e.region as HTMLSelectElement;
     if (cityInput.value === "") {
-      cityInput.value = Preferences.get(P.general._.location_city);
+        cityInput.value = Preferences.get(P.general._.location_city);
     }
     if (regionSelect.selectedIndex === 0) { // no actual selection
-      regionSelect.value = Preferences.get(P.general._.location_region).toString();
+        regionSelect.value = Preferences.get(P.general._.location_region).toString();
     }
     e.saveButton.addEventListener("click", () => {
-      Preferences.set(P.general._.location_city, cityInput.value);
-      const parseResult = P.general._.location_region.fromString(regionSelect.value);
-      if (isString(parseResult)) {
-        log.error(parseResult);
-      } else {
-        Preferences.set(P.general._.location_region, parseResult.value);
-      }
+        Preferences.set(P.general._.location_city, cityInput.value);
+        const parseResult = P.general._.location_region.fromString(regionSelect.value);
+        if (isString(parseResult)) {
+            log.error(parseResult);
+        } else {
+            Preferences.set(P.general._.location_region, parseResult.value);
+        }
     });
 }
