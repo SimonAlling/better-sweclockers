57b9e4e3f71ad58396b969ac7c41c064249069cd
Simon Alling <SimonAlling@users.noreply.github.com>
2020-07-20 10:26:15 +0200

    Move shibe code to its own module (#104)

--- src/operations/logic/editing-tools.tsx
+++ src/operations/logic/editing-tools.tsx
@@ -14,6 +14,8 @@ import * as T from "~src/text";
 import { InsertButtonDescription } from "~src/types";
 import { fromMaybeUndefined, r } from "~src/utilities";
 
+import ACTION_SHIBE from "../actions/shibe";
+
 import * as Smileys from "./smileys";
 import { Action, CursorBehavior, insert, insertIn, placeCursorIn, selectedTextIn, wrapIn, wrap_tag, wrap_verbatim } from "./textarea";
 
@@ -230,26 +232,6 @@ export function toolbarButton(button: Pick<ButtonDescription, "tooltip" | "class
     );
 }
 
-function shibeText(original: string): string {
-    const MAX = 100;
-    return unlines(lines(original).map(
-        line => [
-            BB.start(SITE.TAG.font, CONFIG.CONTENT.shibeFont),
-            BB.start(SITE.TAG.color, CONFIG.CONTENT.shibeColor),
-            BB.start(SITE.TAG.i),
-            CONFIG.NBSP.repeat(randomIntBetween(0, Math.max(0, MAX - line.length))),
-            line,
-            BB.end(SITE.TAG.i),
-            BB.end(SITE.TAG.color),
-            BB.end(SITE.TAG.font),
-        ].join("")
-    ));
-}
-
-function randomIntBetween(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function ACTION_IMG(textarea: HTMLTextAreaElement, undoSupport: boolean): void {
     const selection = selectedTextIn(textarea);
     if (!selection.includes("\n")) {
@@ -334,23 +316,4 @@ function nbspsConfirmationNeeded(selectedText: string): false | number {
     // Replacing a large number of spread-out spaces with NBSPs is both uncommon and time-consuming to restore.
     const confirmationNeeded = numberOfSelectedSpaces > 10 && numberOfSelectedSpaceSegments > 3
     return confirmationNeeded ? numberOfSelectedSpaces : false;
-}
-
-function ACTION_SHIBE(textarea: HTMLTextAreaElement, undoSupport: boolean): void {
-    const selectedText = selectedTextIn(textarea);
-    const selectedLines = lines(selectedText);
-    if (undoSupport || !shibeConfirmationNeeded(selectedLines) || confirm(T.general.shibe_confirm(selectedLines.length))) { // `confirm` is problematic in Chrome (see docs/dialogs.md), but Chrome has full undo support.
-        insertIn(textarea, {
-            string: shibeText(selectedText),
-            replace: true,
-        });
-    }
-}
-
-// A heuristic intended to catch cases when the user likely didn't mean to apply shibe formatting and/or it would be cumbersome to restore the change without undo support.
-function shibeConfirmationNeeded(selectedLines: readonly string[]): boolean {
-    const nonEmptyLines = selectedLines.filter(line => line !== "");
-    const longestLineLength = Math.max(...nonEmptyLines.map(line => line.length));
-    // Shibe formatting is usually applied to a few short lines of text ("wow" etc), and content worth protecting tends to have more and/or longer lines.
-    return nonEmptyLines.length > 5 || longestLineLength > 15;
 }
