3a54a23fc75cd2d8c7cc064306eaab7bdea5a5e8
Simon Alling <alling.simon@gmail.com>
2020-07-20 13:44:54 +0200

    Move search link code to its own module

---
+++ src/operations/actions/search-link.ts
@@ -0,0 +1,17 @@
+import * as BB from "bbcode-tags";
+
+import { SearchEngine, searchURL } from "~src/search-engines";
+import * as SITE from "~src/site";
+
+import { selectedTextIn, wrapIn } from "../logic/textarea";
+
+export default (engine: SearchEngine) => (textarea: HTMLTextAreaElement, _: boolean) => {
+    const tagName = SITE.TAG.url;
+    const selected = selectedTextIn(textarea);
+    const startTag = BB.start(tagName, searchURL(engine, selected));
+    wrapIn(textarea, {
+        before: startTag,
+        after: BB.end(tagName),
+        cursor: selected === "" ? startTag.length - 2 /* for "] */ : "KEEP_SELECTION",
+    });
+}
