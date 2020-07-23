43a10b2b34368f5a3e8a708a73f07d04fff8bf34
Simon Alling <alling.simon@gmail.com>
2020-07-23 09:10:12 +0200

    Fix typo (tool)

--- src/operations/improved-builtin-editing-tools.ts
+++ src/operations/improved-builtin-editing-tools.ts
@@ -6,7 +6,7 @@ export default () => {
     /*
     SweClockers' built-in functions use `.value`, which kills undo history.
     This operation replaces them with our own `text-field-edit`-based versions, thereby reintroducing undo/redo support in supported browsers.
-    Our versions sort of mimic the default ones, but they feature more intuitive behavior, such as keeping the selection when a wrap tool (e.g. B/U/I) tool is used.
+    Our versions sort of mimic the default ones, but they feature more intuitive behavior, such as keeping the selection when a wrap tool (e.g. B/U/I) is used.
 
     We don't care about protecting data in the absence of undo support; I assume SweClockers only ever modify textarea content "safely" (i.e. without deleting anything).
     */
