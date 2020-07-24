057c161f5048c43faa3e6b9bd231785815300f8d
Simon Alling <SimonAlling@users.noreply.github.com>
2020-07-24 17:21:29 +0200

    Update/simplify prevent accidental signout (#123)
    SweClockers has moved to a simpler signout mechanism.
    
    Resolves #121.

--- src/operations/prevent-accidental-signout.ts
+++ src/operations/prevent-accidental-signout.ts
@@ -5,54 +5,9 @@ export default (e: { signoutButtonOrSigninSection: HTMLElement }) => {
     const notLoggedIn = e.signoutButtonOrSigninSection.classList.contains(SITE.CLASS.signinSection);
     if (notLoggedIn) return;
     const signoutButton = e.signoutButtonOrSigninSection;
-    const parent = signoutButton.parentElement as HTMLElement;
-    const safeSignoutForm = signoutButton.cloneNode(true);
-    signoutButton.remove();
-    safeSignoutForm.addEventListener("click", function() {
-        if (confirm(T.general.signout_confirmation)) {
-            const request = new Taiga.Xhr.JsonRpc.Request();
-            request.setUrl(SITE.PATH.SIGNOUT);
-            request.setMethod("signout");
-            request.onError(errorHandler);
-            request.onSuccess(() => window.location.reload());
-            request.send();
+    signoutButton.addEventListener("click", event => {
+        if (!confirm(T.general.signout_confirmation)) {
+            event.stopPropagation();
         }
-    });
-    parent.appendChild(safeSignoutForm);
+    }, true); // Default listener doesn't use capture, so this listener is given precedence.
 };
-
-function errorHandler() {
-    const dialog = new Common.Windows.MessageDialog();
-    dialog.addClass(SITE.CLASS.errorDialog);
-    dialog.setMessage(T.general.signout_error);
-    dialog.setModal(true);
-    dialog.openWindow();
-    dialog.centerOnScreen();
-}
-
-declare namespace Common {
-    namespace Windows {
-        class MessageDialog { // eslint-disable-line @typescript-eslint/no-unused-vars
-            public addClass(x: string): void
-            public setMessage(x: string): void
-            public setModal(x: boolean): void
-            public openWindow(): void
-            public centerOnScreen(): void
-        }
-    }
-}
-
-declare namespace Taiga {
-    namespace Xhr {
-        namespace JsonRpc {
-            class Request { // eslint-disable-line @typescript-eslint/no-unused-vars
-                public setUrl(x: string): void
-                public setMethod(x: string): void
-                public setParams(x: { csrf: string }): void
-                public onError(f: () => void): void
-                public onSuccess(f: () => void): void
-                public send(): void
-            }
-        }
-    }
-}
