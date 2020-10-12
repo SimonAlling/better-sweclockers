bd6f551deeaa30a5a08ae16c59c4637203efa7ed
Simon Alling <alling.simon@gmail.com>
2020-10-12 17:35:48 +0200

    Add static connection to preferences shortcut operation

--- src/operations.ts
+++ src/operations.ts
@@ -51,6 +51,7 @@ const ALWAYS = true;
 
 const improvedBuiltinEditingToolsDescription = "enable improved built-in editing tools";
 const shouldEnableImprovedBuiltinEditingTools = (isInEditMode || isReadingThread) && Preferences.get(P.edit_mode._.improved_builtin_editing_tools);
+const shouldInsertPreferencesShortcut = Preferences.get(P.general._.insert_preferences_shortcut);
 
 // True here means the user wants us to act as if there is undo support (i.e. take no precautions to protect data).
 // Chrome always has actual undo support and Firefox never does.
@@ -171,9 +172,8 @@ const OPERATIONS: readonly Operation<any>[] = [
     }),
     operation({
         description: "insert preferences shortcut",
-        condition: () => !isOnBSCPreferencesPage && Preferences.get(P.general._.insert_preferences_shortcut),
+        condition: () => !isOnBSCPreferencesPage && shouldInsertPreferencesShortcut,
         dependencies: {
-            // Note that, because this operation replaces the signout button, it makes "prevent accidental signout" redundant.
             iconOrSigninButton: SELECTOR.signinButtonOr(SELECTOR.signoutButtonIcon),
             labelOrSigninButton: SELECTOR.signinButtonOr(SELECTOR.signoutButtonLabel),
             signoutButtonOrSigninButton: SELECTOR.signinButtonOr(SELECTOR.signoutButton),
@@ -188,11 +188,18 @@ const OPERATIONS: readonly Operation<any>[] = [
     }),
     operation({
         description: "prevent accidental signout",
-        condition: () => (
-            Preferences.get(P.advanced._.prevent_accidental_signout)
-            &&
-            !Preferences.get(P.general._.insert_preferences_shortcut) // That operation replaces the signout button.
-        ),
+        condition: () => {
+            type OnlyIfPreferencesShortcutDependsOnSignoutButton = (
+                // The preferences shortcut replaces the signout button. Should that change, this operation's condition must be updated.
+                Parameters<typeof insertPreferencesShortcut> extends [{
+                    iconOrSigninButton: HTMLElement,
+                    labelOrSigninButton: HTMLElement,
+                    signoutButtonOrSigninButton: HTMLElement,
+                }] ? boolean : never
+            );
+            const preferencesShortcutIsEnabled: OnlyIfPreferencesShortcutDependsOnSignoutButton = shouldInsertPreferencesShortcut;
+            return !preferencesShortcutIsEnabled && Preferences.get(P.advanced._.prevent_accidental_signout);
+        },
         dependencies: { signoutButtonOrSigninButton: SELECTOR.signinButtonOr(SELECTOR.signoutButton) },
         action: preventAccidentalSignout,
     }),
