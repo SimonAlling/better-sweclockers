04965c2e7baeb7ee3fac8a500c081bbd85ffe61c
SimonAlling <SimonAlling@users.noreply.github.com>
2020-11-01 17:16:34 +0000

    Deploying to gh-pages from  @ da85edbf6a1a40c407ce68c974bfc0d57b017a05 🚀

--- better-sweclockers.meta.js
+++ better-sweclockers.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Better SweClockers
-// @version      2020.10.16.18.25.41
+// @version      2020.11.1.17.16.20
 // @description  Extra functionality and improved usability for the SweClockers website.
 // @author       Simon Alling
 // @namespace    simonalling.se
