7b0345469ceac2a6afdfa04323dfd8314344d878
Simon Alling <alling.simon@gmail.com>
2020-12-03 21:30:07 +0100

    Remove LemonIllusion's adaptive width

--- src/text.ts
+++ src/text.ts
@@ -73,8 +73,6 @@ export const preferences = {
         label: `Allmänt`,
         lock_heights: `Lås höjden på reklam etc`,
         lock_heights_description: `Förhindrar att sidans innehåll hoppar när t ex reklam laddas in`,
-        adaptive_width: `LemonIllusions adaptiva layout`,
-        adaptive_width_description: `Anpassa bredden på huvudinnehållet (t ex artiklar) efter fönstrets bredd`,
         improved_corrections: `Bättre rättelsegränssnitt`,
         improved_corrections_description: `Gör det enklare att skicka in rättelser`,
         insert_preferences_shortcut: `Genväg till inställningar för ${CONFIG.USERSCRIPT_NAME}`,
