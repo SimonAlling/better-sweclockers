628145233d1a5b3c9ad61270bd7f4db6a4ac6f26
Simon Alling <alling.simon@gmail.com>
2021-01-14 23:10:28 +0100

    Use stricter dependencies (caret position)

--- src/operations/caret-position.ts
+++ src/operations/caret-position.ts
@@ -4,12 +4,12 @@ import * as CONFIG from "~src/config";
 import { P, Preferences } from "~src/preferences";
 
 export default (e: {
-    textarea: HTMLElement,
-    previewButton: HTMLElement,
+    textarea: HTMLTextAreaElement,
+    previewButton: HTMLButtonElement,
 }) => {
     // Caret position is only saved when the user requests a preview; it is
     // wiped on any other page unload.
-    const textarea = e.textarea as HTMLTextAreaElement;
+    const textarea = e.textarea;
     const savedPosition = Storage.get_session(CONFIG.KEY.caret_position, NaN).value;
     // The check below is done here instead of as a condition for the entire
     // operation, because we need the wiping to be done even if the user has
