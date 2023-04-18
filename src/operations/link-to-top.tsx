eafe796b7b82fc22a966fcfd09239f4fb452397e
Simon Alling <SimonAlling@users.noreply.github.com>
2023-04-18 23:15:19 +0200

    Fix link to top icon (#246)
    I noticed that it was broken (no icon shown) after merging #245.

--- src/operations/link-to-top.tsx
+++ src/operations/link-to-top.tsx
@@ -8,7 +8,7 @@ export default (e: { parent: HTMLElement }) => {
     renderIn(e.parent, insertAtTheEnd, (
         <a href="javascript:window.scrollTo(0, 0)" title={T.general.link_to_top_tooltip}>
             <svg class={SITE.CLASS.icon} style="transform: rotate(-90deg);">
-                <use xlinkHref="#icon_double_arrow_right" />
+                <use xlinkHref="/gfx/iconmap.svg?v=20221125#icon_double_arrow_right" />
             </svg>
         </a>
     ));
