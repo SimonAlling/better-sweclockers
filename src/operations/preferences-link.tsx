401b980ea29f4057bff8277f31d1547512863d95
Simon Alling <alling.simon@gmail.com>
2023-11-17 08:27:04 +0100

    Patch preferences link
    💡 `git show --color-words=.`

--- src/operations/preferences-link.tsx
+++ src/operations/preferences-link.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { h } from "preact";
 
 import * as CONFIG from "~src/config";
@@ -8,7 +9,7 @@ import * as T from "~src/text";
 export default (e: { settingsNavigation: HTMLElement }) => {
     renderIn(e.settingsNavigation, insertAtTheEnd, (
         <li title={T.general.preferences_link} class={SITE.CLASS.menuItem}>
-            <a href={CONFIG.PATH.PREFERENCES.link(SITE.PATH.SETTINGS.link)} class={SITE.CLASS.link}>
+            <a href={CONFIG.PATH.PREFERENCES.link(SITE.PATH.SETTINGS.link)} class={classNames(SITE.CLASS.button, SITE.CLASS.link)}>
                 <span class={SITE.CLASS.icon}></span>
                 {" "} {/* SweClockers relies on whitespace for spacing here. */}
                 <span class={SITE.CLASS.label}>{T.general.preferences_link}</span>
